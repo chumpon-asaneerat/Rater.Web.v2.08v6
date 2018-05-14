5e8615a236496528dcc77ed21f8e60dd535b4ba2
chumpon-asaneerat <chumponsenate@gmail.com>
2018-05-14 19:34:56 +0700

    Update db script 2018-05-15

--- db/scripts/dist/update-2018-05-15.sql
+++ db/scripts/dist/update-2018-05-15.sql
@@ -39,6 +39,685 @@ GO
 
 -- =============================================
 -- Author: Chumpon Asaneerat
+-- Name: GetLanguages.
+-- Description:	Gets languages.
+-- [== History ==]
+-- <2017-05-31> :
+--	- Stored Procedure Created.
+-- <2018-04-16> :
+--	- The @enabled parameter default value is NULL.
+-- <2018-05-15> :
+--	- change column LangId to langId
+--	- change column FlagId to flagId
+--
+-- [== Example ==]
+--
+--exec GetLanguages; -- for get all.
+--exec GetLanguages 1; -- for only enabled language.
+--exec GetLanguages 0; -- for only disabled language.
+-- =============================================
+ALTER PROCEDURE [dbo].[GetLanguages]
+(
+    @enabled bit = null
+)
+AS
+BEGIN
+    SELECT langId
+		 , flagId
+		 , DescriptionEN
+		 , DescriptionNative 
+		 , SortOrder
+		 , Enabled
+      FROM [dbo].[LanguageView]
+     WHERE [ENABLED] = COALESCE(@enabled, [ENABLED])
+     ORDER BY SortOrder
+END
+
+GO
+
+
+/*********** Script Update Date: 2018-05-15  ***********/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	GetMemberTypes
+-- [== History ==]
+-- <2017-05-31> :
+--	- Stored Procedure Created.
+-- <2018-04-16> :
+--	- change language id from nvarchar(10) to nvarchar(3).
+-- <2018-05-15> :
+--	- change column LangId to langId
+--	- change column MemberTypeId to memberTypeId
+--
+-- [== Example ==]
+--
+--exec GetMemberTypes NULL, 1;  -- for only enabled languages.
+--exec GetMemberTypes;          -- for get all.
+--exec GetMemberTypes N'EN';    -- for get MemberType for EN language.
+--exec GetMemberTypes N'TH';    -- for get MemberType for TH language.
+-- =============================================
+ALTER PROCEDURE [dbo].[GetMemberTypes] 
+(
+  @langId nvarchar(3) = NULL
+, @enabled bit = NULL
+)
+AS
+BEGIN
+	SELECT langId
+		 , memberTypeId
+		 , MemberTypeDescriptionEN
+		 , MemberTypeDescriptionNavive
+		 , SortOrder
+		 , Enabled 
+	  FROM MemberTypeMLView
+	 WHERE [ENABLED] = COALESCE(@enabled, [ENABLED])
+	   AND UPPER(LTRIM(RTRIM(LangId))) = UPPER(LTRIM(RTRIM(COALESCE(@langId,LangId))))
+	 ORDER BY SortOrder, MemberTypeId
+END
+
+GO
+
+
+/*********** Script Update Date: 2018-05-15  ***********/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Name: GetPeriodUnits.
+-- Description:	Get Period Units.
+-- [== History ==]
+-- <2017-05-31> :
+--	- Stored Procedure Created.
+-- <2018-04-16> :
+--	- change language id from nvarchar(10) to nvarchar(3).
+-- <2018-05-15> :
+--	- change column LangId to langId
+--	- change column PeriodUnitId to periodUnitId
+--
+-- [== Example ==]
+--
+--exec GetPeriodUnits NULL, 1;  -- for only enabled languages.
+--exec GetPeriodUnits;          -- for get all.
+--exec GetPeriodUnits N'EN';    -- for get PeriodUnit for EN language.
+-- =============================================
+ALTER PROCEDURE [dbo].[GetPeriodUnits] 
+(
+  @langId nvarchar(3) = NULL
+, @enabled bit = NULL
+)
+AS
+BEGIN
+	SELECT langId
+		 , periodUnitId
+		 , PeriodUnitDescriptionEN
+		 , PeriodUnitDescriptionNative
+		 , SortOrder
+		 , Enabled 
+	  FROM PeriodUnitMLView
+	 WHERE [ENABLED] = COALESCE(@enabled, [ENABLED])
+	   AND UPPER(LTRIM(RTRIM(LangId))) = UPPER(LTRIM(RTRIM(COALESCE(@langId,LangId))))
+	 ORDER BY SortOrder, PeriodUnitId
+END
+
+GO
+
+
+/*********** Script Update Date: 2018-05-15  ***********/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	GetLimitUnits
+-- [== History ==]
+-- <2017-05-31> :
+--	- Stored Procedure Created.
+-- <2018-04-16> :
+--	- change language id from nvarchar(10) to nvarchar(3).
+-- <2018-05-15> :
+--	- change column LangId to langId
+--	- change column LimitUnitId to limitUnitId
+--
+-- [== Example ==]
+--
+--exec GetLimitUnits NULL, 1;  -- for only enabled languages.
+--exec GetLimitUnits;          -- for get all.
+--exec GetLimitUnits N'EN';    -- for get LimitUnit for EN language.
+-- =============================================
+ALTER PROCEDURE [dbo].[GetLimitUnits] 
+(
+  @langId nvarchar(3) = NULL
+, @enabled bit = NULL
+)
+AS
+BEGIN
+	SELECT langId
+		 , limitUnitId
+		 , LimitUnitDescriptionEN
+		 , LimitUnitDescriptionNative
+		 , LimitUnitTextEN
+		 , LimitUnitTextNative
+		 , SortOrder
+		 , Enabled
+	  FROM LimitUnitMLView
+	 WHERE [ENABLED] = COALESCE(@enabled, [ENABLED])
+	   AND UPPER(LTRIM(RTRIM(LangId))) = UPPER(LTRIM(RTRIM(COALESCE(@langId,LangId))))
+	 ORDER BY SortOrder, LimitUnitId
+END
+
+GO
+
+
+/*********** Script Update Date: 2018-05-15  ***********/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	GetLicenses
+-- [== History ==]
+-- <2017-05-31> :
+--	- Stored Procedure Created.
+-- <2018-04-16> :
+--	- change language id from nvarchar(10) to nvarchar(3).
+-- <2018-05-15> :
+--	- change column LangId to langId
+--	- change column LicenseTypeId to licenseTypeId
+--	- change column PeriodUnitId to periodUnitId
+--
+-- [== Example ==]
+--
+--exec GetLicenseTypes N'EN'; -- for only EN language.
+--exec GetLicenseTypes;       -- for get all.
+-- =============================================
+ALTER PROCEDURE [dbo].[GetLicenseTypes] 
+(
+  @langId nvarchar(3) = null
+)
+AS
+BEGIN
+	SELECT langId
+		 , licenseTypeId
+		 , LicenseTypeDescriptionEn
+		 , LicenseTypeDescriptionNative
+		 , AdTextEN
+		 , AdTextNative
+		 , periodUnitId
+		 , NumberOfUnit
+		 , UseDefaultPrice
+		 , Price
+		 , CurrencySymbol
+		 , CurrencyText
+		 , SortOrder
+		 , Enabled 
+	  FROM LicenseTypeMLView
+	 WHERE langId = COALESCE(@langId, langId)
+	   AND Enabled = 1
+	 Order By SortOrder, LangId, LicenseTypeId;
+END
+
+GO
+
+
+/*********** Script Update Date: 2018-05-15  ***********/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Name: GetLicenseFeatures.
+-- Description:	Get License Features.
+-- [== History ==]
+-- <2017-05-31> :
+--	- Stored Procedure Created.
+-- <2018-04-16> :
+--	- change language id from nvarchar(10) to nvarchar(3).
+-- <2018-05-15> :
+--	- change column LangId to langId
+--	- change column LicenseTypeId to licenseTypeId
+--	- change column LimitUnitId to limitUnitId
+--
+-- [== Example ==]
+--
+--exec GetLicenseFeatures N'EN';    -- for only EN language.
+--exec GetLicenseFeatures;          -- for get all.
+--exec GetLicenseFeatures N'EN', 1; -- for all features for LicenseTypeId = 1 in EN language.
+--exec GetLicenseFeatures N'TH', 0; -- for all features for LicenseTypeId = 0 in TH language.
+-- =============================================
+ALTER PROCEDURE [dbo].[GetLicenseFeatures] 
+(
+  @langId nvarchar(3) = null
+, @licenseTypeId int = null
+)
+AS
+BEGIN
+	SELECT langId
+		 , licenseTypeId
+		 , seq
+		 , limitUnitId
+		 , LimitUnitDescriptionEN
+		 , LimitUnitDescriptionNative
+		 , NoOfLimit
+		 , LimitUnitTextEN
+		 , LimitUnitTextNative
+		 , SortOrder
+		 , Enabled 
+	  FROM LicenseFeatureMLView
+	 WHERE langId = COALESCE(@langId, langId)
+	   AND Enabled = 1
+	   AND LicenseTypeId = COALESCE(@licenseTypeId, LicenseTypeId)
+	 Order By SortOrder, LangId, LicenseTypeId;
+END
+
+GO
+
+
+/*********** Script Update Date: 2018-05-15  ***********/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Name: GetLicenses.
+-- Description:	Get Licenses.
+-- [== History ==]
+-- <2018-05-09> :
+--	- Stored Procedure Created.
+-- <2018-05-15> :
+--	- change column LangId to langId
+--	- change column LicenseTypeId to licenseTypeId
+--	- change column PeriodUnitId to periodUnitId
+--  - change column NumberOfUnit to NoOfUnit
+--	- change column LimitUnitId to limitUnitId
+--
+-- [== Example ==]
+--
+--exec GetLicenses N'EN';    -- for only EN language.
+--exec GetLicenses;          -- for get all.
+-- =============================================
+ALTER PROCEDURE [dbo].[GetLicenses] 
+(
+  @langId nvarchar(3) = null
+, @licenseTypeId int = null  
+)
+AS
+BEGIN
+	SELECT langId
+		 , licenseTypeId
+		 , seq
+		 , LicenseTypeDescriptionEn
+		 , LicenseTypeDescriptionNative
+		 , AdTextEN
+		 , AdTextNative
+		 , periodUnitId
+		 , NumberOfUnit as NoOfUnit
+		 , UseDefaultPrice
+		 , Price
+		 , CurrencySymbol
+		 , CurrencyText
+		 , limitUnitId
+		 , LimitUnitDescriptionEN
+		 , LimitUnitDescriptionNative
+		 , NoOfLimit
+		 , LimitUnitTextEN
+		 , LimitUnitTextNative
+		 , SortOrder
+		 , Enabled
+	  FROM LicenseMLView
+	 WHERE langId = COALESCE(@langId, langId)
+	   AND LicenseTypeId = COALESCE(@licenseTypeId, LicenseTypeId)
+	   AND Enabled = 1
+	 Order By SortOrder, LicenseTypeId, Seq
+END
+
+GO
+
+
+/*********** Script Update Date: 2018-05-15  ***********/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	GetUserInfos
+-- [== History ==]
+-- <2017-05-31> :
+--	- Stored Procedure Created.
+-- <2018-04-16> :
+--	- change language id from nvarchar(10) to nvarchar(3).
+-- <2018-05-15> :
+--	- change column LangId to langId
+--	- change column UserId to userId
+--
+-- [== Example ==]
+--
+--exec GetUserInfos NULL, NULL, 1;  -- for only enabled languages.
+--exec GetUserInfos;                -- for get all.
+--exec GetUserInfos N'EN', NULL;    -- for get UserInfo for EN language all member type.
+--exec GetUserInfos N'TH', NULL;    -- for get UserInfo for TH language all member type.
+--exec GetUserInfos N'EN', 100;     -- for get UserInfo for EN language member type = 100.
+--exec GetUserInfos N'TH', 180;     -- for get UserInfo for TH language member type = 180.
+-- =============================================
+ALTER PROCEDURE [dbo].[GetUserInfos] 
+(
+  @langId nvarchar(3) = NULL
+, @memberType int = NULL
+, @enabled bit = NULL
+)
+AS
+BEGIN
+	SELECT langId
+		 , userId
+		 , MemberType
+		 , PrefixEN
+		 , FirstNameEn
+		 , LastNameEn
+		 , FullNameEN
+		 , PrefixNative
+		 , FirstNameNative
+		 , LastNameNative
+		 , FullNameNative
+		 , UserName
+		 , Password
+		 , ObjectStatus
+		 , SortOrder
+		 , Enabled 
+	  FROM UserInfoMLView
+	 WHERE [ENABLED] = COALESCE(@enabled, [ENABLED])
+	   AND [MemberType] = COALESCE(@memberType, [MemberType])
+	   AND UPPER(LTRIM(RTRIM(LangId))) = UPPER(LTRIM(RTRIM(COALESCE(@langId,LangId))))
+	 ORDER BY SortOrder, UserId
+END
+
+GO
+
+
+/*********** Script Update Date: 2018-05-15  ***********/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	GetCustomers
+-- [== History ==]
+-- <2017-05-31> :
+--	- Stored Procedure Created.
+-- <2018-04-> :
+--	- change language id from nvarchar(10) to nvarchar(3).
+-- <2018-05-15> :
+--	- change column LangId to langId
+--
+-- [== Example ==]
+--
+--exec GetCustomers NULL, NULL, 1;             -- for only enabled languages.
+--exec GetCustomers;                           -- for get all.
+--exec GetCustomers N'EN';                     -- for get customers for EN language.
+--exec GetCustomers N'TH';                     -- for get customers for TH language.
+--exec GetCustomers N'TH', N'EDL-C2017060011'; -- for get customer for TH language by Customer Id.
+-- =============================================
+ALTER PROCEDURE [dbo].[GetCustomers] 
+(
+  @langId nvarchar(3) = NULL
+, @customerId nvarchar(30) = NULL
+, @enabled bit = NULL
+)
+AS
+BEGIN
+	SELECT langId
+		 , customerId
+		 , CustomerNameEN
+		 , CustomerNameNative
+		 , TaxCodeEN
+		 , TaxCodeNative
+		 , Address1EN
+		 , Address1Native
+		 , Address2EN
+		 , Address2Native
+		 , CityEN
+		 , CityNative
+		 , ProvinceEN
+		 , ProvinceNative
+		 , PostalCodeEN
+		 , PostalCodeNative
+		 , Phone
+		 , Mobile
+		 , Fax
+		 , Email
+		 , ObjectStatus
+		 , SortOrder
+		 , Enabled 
+	  FROM CustomerMLView
+	 WHERE [ENABLED] = COALESCE(@enabled, [ENABLED])
+	   AND UPPER(LTRIM(RTRIM(LangId))) = UPPER(LTRIM(RTRIM(COALESCE(@langId, LangId))))
+	   AND UPPER(LTRIM(RTRIM(CustomerId))) = UPPER(LTRIM(RTRIM(COALESCE(@customerId, CustomerId))))
+	 ORDER BY SortOrder, CustomerId
+END
+
+GO
+
+
+/*********** Script Update Date: 2018-05-15  ***********/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	GetMemberInfos
+-- [== History ==]
+-- <2017-05-31> :
+--	- Stored Procedure Created.
+-- <2018-04-16> :
+--	- change language id from nvarchar(10) to nvarchar(3).
+-- <2018-05-15> :
+--	- change column LangId to langId
+--	- change column CustomerId to customerId
+--	- change column MemberId to memberId
+--
+-- [== Example ==]
+--
+--exec GetMemberInfos NULL, NULL, NULL, 1;                  -- for only enabled languages.
+--exec GetMemberInfos;                                      -- for get all.
+--exec GetMemberInfos N'EN';                                -- for get MemberInfos for EN language.
+--exec GetMemberInfos N'TH';                                -- for get MemberInfos for TH language.
+--exec GetMemberInfos N'TH', N'EDL-C2017060011';            -- for get MemberInfos by CustomerID.
+--exec GetMemberInfos N'TH', N'EDL-C2017060011', N'M00001'; -- for get MemberInfo by CustomerID and MemberId.
+-- =============================================
+ALTER PROCEDURE [dbo].[GetMemberInfos] 
+(
+  @langId nvarchar(3) = NULL
+, @customerId nvarchar(30) = NULL
+, @memberId nvarchar(30) = NULL
+, @enabled bit = NULL
+)
+AS
+BEGIN
+	SELECT langId
+		 , customerId
+		 , memberId
+		 , MemberType
+		 , PrefixEN
+		 , FirstNameEN
+		 , LastNameEN
+		 , FullNameEN
+		 , PrefixNative
+		 , FirstNameNative
+		 , LastNameNative
+		 , FullNameNative
+		 , IDCard
+		 , TagId
+		 , EmployeeCode
+		 , UserName
+		 , Password
+		 , ObjectStatus
+		 , SortOrder
+		 , Enabled 
+	  FROM MemberInfoMLView
+	 WHERE [ENABLED] = COALESCE(@enabled, [ENABLED])
+	   AND UPPER(LTRIM(RTRIM(LangId))) = UPPER(LTRIM(RTRIM(COALESCE(@langId, LangId))))
+	   AND UPPER(LTRIM(RTRIM(CustomerId))) = UPPER(LTRIM(RTRIM(COALESCE(@customerId, CustomerId))))
+	   AND UPPER(LTRIM(RTRIM(MemberId))) = UPPER(LTRIM(RTRIM(COALESCE(@memberId, MemberId))))
+	 ORDER BY SortOrder, LangId, CustomerId, MemberId;
+END
+
+GO
+
+
+/*********** Script Update Date: 2018-05-15  ***********/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Name: GetBranchs.
+-- Description:	Get Branchs.
+-- [== History ==]
+-- <2017-05-31> :
+--	- Stored Procedure Created.
+-- <2018-04-16> :
+--	- change language id from nvarchar(10) to nvarchar(3).
+-- <2018-05-15> :
+--	- change column LangId to langId
+--	- change column CustomerId to customerId
+--	- change column BranchId to branchId
+--
+-- [== Example ==]
+--
+--exec GetBranchs NULL, NULL, NULL, 1;                 -- for only enabled languages.
+--exec GetBranchs;                                     -- for get all.
+--exec GetBranchs N'EN';                               -- for get Branchs for EN language.
+--exec GetBranchs N'TH';                               -- for get Branchs for TH language.
+--exec GetBranchs N'TH', N'EDL-C2017060011';           -- for get Branchs by CustomerID.
+--exec GetBranchs N'TH', N'EDL-C2017060011', N'B0001'; -- for get Branch by CustomerID and BranchId.
+-- =============================================
+ALTER PROCEDURE [dbo].[GetBranchs] 
+(
+  @langId nvarchar(3) = NULL
+, @customerId nvarchar(30) = NULL
+, @branchId nvarchar(30) = NULL
+, @enabled bit = NULL
+)
+AS
+BEGIN
+	SELECT langId
+		 , customerId
+		 , branchId
+		 , BranchNameEN
+		 , BranchNameNative
+		 , ObjectStatus
+		 , SortOrder
+		 , Enabled 
+	  FROM BranchMLView
+	 WHERE [ENABLED] = COALESCE(@enabled, [ENABLED])
+	   AND UPPER(LTRIM(RTRIM(LangId))) = UPPER(LTRIM(RTRIM(COALESCE(@langId, LangId))))
+	   AND UPPER(LTRIM(RTRIM(CustomerId))) = UPPER(LTRIM(RTRIM(COALESCE(@customerId, CustomerId))))
+	   AND UPPER(LTRIM(RTRIM(BranchId))) = UPPER(LTRIM(RTRIM(COALESCE(@branchId, BranchId))))
+	 ORDER BY SortOrder, LangId, CustomerId, BranchId;
+END
+
+GO
+
+
+/*********** Script Update Date: 2018-05-15  ***********/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Name: GetOrgs
+-- Description:	Get Organizations.
+-- [== History ==]
+-- <2017-05-31> :
+--	- Stored Procedure Created.
+-- <2018-04-16> :
+--	- change language id from nvarchar(10) to nvarchar(3).
+-- <2018-05-15> :
+--	- change column LangId to langId
+--	- change column CustomerId to customerId
+--	- change column OrgId to orgId
+--	- change column ParentId to parentId
+--	- change column BranchId to branchId
+--
+-- [== Example ==]
+--
+--/* Get All */
+--exec GetOrgs NULL, NULL, NULL, NULL, 1; -- enabled languages only.
+--exec GetOrgs; -- all languages.
+--/* With Specificed CustomerId */
+--exec GetOrgs N'EN', N'EDL-C2017060008'; -- Gets Orgs EN language.
+--exec GetOrgs N'TH', N'EDL-C2017060008'; -- Gets Orgs TH language.
+--/* With Specificed CustomerId, BranchId */
+--exec GetOrgs N'EN', N'EDL-C2017060008', N'B0001'; -- Gets EN language in Branch 1.
+--exec GetOrgs N'TH', N'EDL-C2017060008', N'B0002'; -- Gets TH language in Branch 2.
+-- =============================================
+ALTER PROCEDURE [dbo].[GetOrgs] 
+(
+  @langId nvarchar(3) = NULL
+, @customerId nvarchar(30) = NULL
+, @branchId nvarchar(30) = NULL
+, @orgId nvarchar(30) = NULL
+, @enabled bit = NULL
+)
+AS
+BEGIN
+	SELECT langId
+		 , customerId
+		 , orgId
+		 , parentId
+		 , branchId
+		 , OrgNameEN
+		 , BranchNameEN
+		 , OrgNameNative
+		 , BranchNameNative
+		 , OrgStatus
+		 , BranchStatus
+		 , SortOrder
+		 , Enabled 
+	  FROM OrgMLView
+	 WHERE [ENABLED] = COALESCE(@enabled, [ENABLED])
+	   AND UPPER(LTRIM(RTRIM(LangId))) = UPPER(LTRIM(RTRIM(COALESCE(@langId, LangId))))
+	   AND UPPER(LTRIM(RTRIM(CustomerId))) = UPPER(LTRIM(RTRIM(COALESCE(@customerId, CustomerId))))
+	   AND UPPER(LTRIM(RTRIM(BranchId))) = UPPER(LTRIM(RTRIM(COALESCE(@branchId, BranchId))))
+	   AND UPPER(LTRIM(RTRIM(OrgId))) = UPPER(LTRIM(RTRIM(COALESCE(@orgId, OrgId))))
+	 ORDER BY SortOrder, LangId, CustomerId, BranchId, OrgId
+END
+
+GO
+
+
+/*********** Script Update Date: 2018-05-15  ***********/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
 -- Description:	Get Raw Votes.
 -- [== History ==]
 -- <2016-10-30> :
