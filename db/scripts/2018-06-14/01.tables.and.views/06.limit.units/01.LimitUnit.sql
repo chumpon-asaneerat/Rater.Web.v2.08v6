d61648003a3955341c970e7a1d25138d6edaaf7b
chumpon-asaneerat <chumponsenate@gmail.com>
2018-06-12 23:05:55 +0700

    Update 2018-06-12#16 ## Update db script.    - LimitUnit table change column name.

--- db/scripts/2018-06-14/01.tables.and.views/06.limit.units/01.LimitUnit.sql
+++ db/scripts/2018-06-14/01.tables.and.views/06.limit.units/01.LimitUnit.sql
@@ -7,7 +7,7 @@ GO
 CREATE TABLE [dbo].[LimitUnit](
 	[LimitUnitId] [int] NOT NULL,
 	[LangId] [nvarchar](3) NOT NULL,
-	[LimitDesc] [nvarchar](100) NOT NULL,
+	[LimitName] [nvarchar](100) NOT NULL,
 	[UnitText] [nvarchar](20) NULL,
  CONSTRAINT [PK_LimitUnit] PRIMARY KEY CLUSTERED 
 (
@@ -24,8 +24,8 @@ GO
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'The ISO 639-1 alpha 2 code.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'LimitUnit', @level2type=N'COLUMN',@level2name=N'LangId'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'The English Description for LimitUnit.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'LimitUnit', @level2type=N'COLUMN',@level2name=N'LimitDesc'
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'The Name for LimitUnit.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'LimitUnit', @level2type=N'COLUMN',@level2name=N'LimitName'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'The English limit unit text.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'LimitUnit', @level2type=N'COLUMN',@level2name=N'UnitText'
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'The limit unit text.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'LimitUnit', @level2type=N'COLUMN',@level2name=N'UnitText'
 GO
