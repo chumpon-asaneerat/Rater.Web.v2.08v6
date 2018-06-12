521747db4e11d7d8466b649e81be59a8c78b3882
chumpon-asaneerat <chumponsenate@gmail.com>
2018-06-12 23:22:12 +0700

    Update 2018-06-12#20 ## Update db script.    - NewSID (FN). change column size from 80 to 40.

--- db/scripts/2018-06-14/02.functions/01.utilities/05.NewSID.sql
+++ db/scripts/2018-06-14/02.functions/01.utilities/05.NewSID.sql
@@ -14,18 +14,20 @@ GO
 -- [== History ==]
 -- <2018-05-29> :
 --	- Stored Procedure Created.
+-- <2018-06-12> :
+--	- Change column size from 80 to 40.
 --
 -- [== Example ==]
 --
 -- =============================================
 CREATE FUNCTION [dbo].[NewSID]()
-RETURNS nvarchar(80)
+RETURNS nvarchar(40)
 AS
 BEGIN
 DECLARE @id uniqueidentifier;
-DECLARE @result nvarchar(80);
+DECLARE @result nvarchar(40);
 	SELECT @id = NEW_ID FROM NewIDView;
-    SELECT @result = CONVERT(nvarchar(80), @id);
+    SELECT @result = CONVERT(nvarchar(40), @id);
     RETURN @result;
 END
 
