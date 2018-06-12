2cb1eea09b572d0feb183739ef83efa76f8c2378
chumpon-asaneerat <chumponsenate@gmail.com>
2018-06-12 23:10:16 +0700

    Update 2018-06-12#18 ## Update db script.    - MemberType table change columns name.

--- db/scripts/2018-06-14/01.tables.and.views/04.member.types/01.MemberType.sql
+++ db/scripts/2018-06-14/01.tables.and.views/04.member.types/01.MemberType.sql
@@ -5,17 +5,23 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE [dbo].[MemberType](
-	[MTypeId] [int] NOT NULL,
+	[MemberTypeId] [int] NOT NULL,
 	[LangId] [nvarchar](3) NOT NULL,
-	[MTypeDesc] [nvarchar](50) NOT NULL,
+	[TypeName] [nvarchar](50) NOT NULL,
  CONSTRAINT [PK_MemberType] PRIMARY KEY CLUSTERED 
 (
-	[MTypeId] ASC,
+	[MemberTypeId] ASC,
 	[LangId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
 GO
 
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'The Member Type Id (PK).' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'MemberType', @level2type=N'COLUMN',@level2name=N'MemberTypeId'
+GO
+
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'The ISO 639-1 alpha 2 code.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'MemberType', @level2type=N'COLUMN',@level2name=N'LangId'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'The Member Type Name' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'MemberType', @level2type=N'COLUMN',@level2name=N'TypeName'
 GO
